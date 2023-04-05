1b52e91bd313a88433cf7310a124ba7c6b3c3734
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-05 22:01:49 +0900

    Scaffold & Material App

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.amber
+          ),
+        ),
       home: Scaffold(
         appBar: AppBar(title: const Text("Flutter Basic"),), 
         body: const Center(
           child: Text("Hello World")
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => debugPrint("clicked"),
+          child: const Icon(Icons.ac_unit)),
         ),
     );
   }
