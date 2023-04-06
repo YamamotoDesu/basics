dc9a16d3701fab335841f08c6164d546a5f7fc07
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-06 12:24:54 +0900

    Widgets - Layout Builder

--- lib/main.dart
+++ lib/main.dart
@@ -115,14 +115,21 @@ class MyApp extends StatelessWidget {
                   const SizedBox(
                     height: 40,
                   ),
-                  Builder(builder: (context){
-                    return Container(
-                      color: Colors.green,
-                      width: MediaQuery.of(context).size.width,
-                      height: 100,
-                      child: const Text("MediaQuery Example"),
-                    );
-                  })
+                  Container(
+                    height: 300,
+                    width: 300,
+                    color: Colors.yellow,
+                    child: LayoutBuilder(builder: (context, constraints) {
+                      return Center(
+                        child: Container(
+                          color: Colors.blue,
+                          width: constraints.maxWidth * 0.8,
+                          height: constraints.maxHeight / 2,
+                          child: const Text("Layout Builder Example"),
+                        ),
+                      );
+                    }),
+                  ),
                 ]),
           ),
         ),
