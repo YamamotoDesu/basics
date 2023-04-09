5788917a88924134b8b04b227d6f8fe7203616fb
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-09 16:12:30 +0900

    Animations II - Sunrise Sun, Moon & Stars

--- lib/presentation/theme_animation/theme_animation_screen.dart
+++ lib/presentation/theme_animation/theme_animation_screen.dart
@@ -2,6 +2,10 @@ import 'package:basics/application/theme_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'widgets/moonstart.dart';
+import 'widgets/star.dart';
+import 'widgets/sun.dart';
+
 class ThemeAnimationScreen extends StatelessWidget {
   const ThemeAnimationScreen({super.key});
 
@@ -25,11 +29,12 @@ class ThemeAnimationScreen extends StatelessWidget {
                   decoration: BoxDecoration(
                     boxShadow: [
                       BoxShadow(
-                        color: themeService.isDarkModeOn? Colors.black87 : Colors.grey,
-                        offset: const Offset(0,3),
-                        blurRadius: 5,
-                        spreadRadius: 7
-                      )
+                          color: themeService.isDarkModeOn
+                              ? Colors.black87
+                              : Colors.grey,
+                          offset: const Offset(0, 3),
+                          blurRadius: 5,
+                          spreadRadius: 7)
                     ],
                     borderRadius: BorderRadius.circular(15),
                     gradient: LinearGradient(
@@ -49,6 +54,55 @@ class ThemeAnimationScreen extends StatelessWidget {
                   ),
                   child: Stack(
                     children: [
+                      Positioned(
+                        top: 70,
+                        right: 50,
+                        child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 200),
+                            opacity: themeService.isDarkModeOn ? 1 : 0,
+                            child: const Star()),
+                      ),
+                      Positioned(
+                        top: 150,
+                        left: 60,
+                        child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 200),
+                            opacity: themeService.isDarkModeOn ? 1 : 0,
+                            child: const Star()),
+                      ),
+                      Positioned(
+                        top: 40,
+                        left: 100,
+                        child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 200),
+                            opacity: themeService.isDarkModeOn ? 1 : 0,
+                            child: const Star()),
+                      ),
+                      Positioned(
+                        top: 50,
+                        left: 50,
+                        child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 200),
+                            opacity: themeService.isDarkModeOn ? 1 : 0,
+                            child: const Star()),
+                      ),
+                      AnimatedPositioned(
+                        duration: const Duration(milliseconds: 400),
+                        top: themeService.isDarkModeOn ? 100 : 130,
+                        right: themeService.isDarkModeOn ? 100 : -40,
+                        child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 300),
+                            opacity: themeService.isDarkModeOn ? 1 : 0,
+                            child: const Moon()),
+                      ),
+                      AnimatedPadding(
+                        padding: EdgeInsets.only(
+                            top: themeService.isDarkModeOn ? 110 : 50),
+                        duration: const Duration(milliseconds: 200),
+                        child: const Center(
+                          child: Sun(),
+                        ),
+                      ),
                       Align(
                         alignment: Alignment.bottomCenter,
                         child: Container(
